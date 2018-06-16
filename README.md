0336d967a720d2638b38e966601130a0825b382c
Robert  Povelones <robert@rpdesignlab.com>
2018-06-15 23:44:08 -0400

    add core class

---
+++ README.md
@@ -0,0 +1,38 @@
+# WordPress Admin Columns
+
+An easy to use API for adding columns in the admin area.
+
+## Setup
+Simply specify your post type and the columns you want to add via the main `RPD_Admin_Columns` class. Then hook into `after_setup_theme` to register your columns.
+
+```php
+function add_admin_columns() {
+
+  $my_post_type = new RPD_Admin_Columns( 'my_post_type', [
+    [
+      'key'      => 'my_custom_column_key', // a unique key for this column
+      'title'    => 'my_column_title',      // the display title
+      'callback' => 'my_column_callback',   // name of function to render the column contents
+      'sortable' => 1,                      // true/false - should column be sortable?
+      'sort_key' => 'my_column_meta_key'    // the meta_key for sorting
+    ],
+    [
+      'key'      => 'my_custom_col_2',
+      'title'    => 'col_2_title',
+      'callback' => 'col_2_callback'
+    ]
+  ]);
+  $my_post_type->register_columns();
+
+}
+add_action('after_setup_theme', __NAMESPACE__ . '\\add_admin_columns');
+```
+
+## Callbacks
+The callback function is where you specify what actually gets output in the column. Each column can have a unique callback or share one with another column.
+
+```php
+function my_column_callback() {
+  // render column contents here
+}
+```
---
+++ rpd-admin-columns.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * RPD Admin Columns.
+ * A nicer way of adding columns in the admin screen.
+ * Initialize with a post type and an array of custom columns.
+ *
+ * @var string | $post_type
+ * @var array | $cols_to_add
+ */
+class RPD_Admin_Columns {
+
+  /**
+   * The post type we are adding columns to.
+   */
+  public $post_type;
+
+  /**
+   * The columns we are adding.
+   * This variable will be used throughout the class.
+   */
+  public $cols_to_add = [];
+
+  /**
+   * Any pre-existing columns to remove.
+   */
+  public $cols_to_unset = [];
+
+  /**
+   * Construct.
+   */
+  public function __construct( $post_type, $cols_to_add ) {
+    $this->post_type = $post_type;
+    $this->cols_to_add = $cols_to_add;
+  }
+
+  /**
+   * Register our custom columns.
+   */
+  public function register_columns() {
+    // add/remove columns
+    add_filter("manage_{$this->post_type}_posts_columns", array( $this, "manage_columns" ), 5);
+    // add content to custom columns
+    add_action("manage_{$this->post_type}_posts_custom_column", array( $this, "render_columns" ), 5, 2);
+    // make custom columns sortable
+    add_action("manage_edit-{$this->post_type}_sortable_columns", array( $this, "make_columns_sortable" ), 5, 2);
+    // add query order for custom sortable columns
+    add_action("pre_get_posts", array( $this, "admin_query_ordering" ), 5, 2);
+  }
+
+  /**
+   * Custom columns to add.
+   *
+   * @hooked manage_{$post_type}_posts_columns.
+   */
+  public function manage_columns( $cols ) {
+    // add custom columns
+    foreach ( $this->cols_to_add as $col ) {
+      $key = $col['key'];
+      $cols[$key] = $col['title'];
+    }
+    // remove columns (if necessary)
+    foreach ( $this->cols_to_unset as $col ) {
+      unset($cols[$col]);
+    }
+    return $cols;
+  }
+
+  /**
+   * Render each of our custom column content.
+   * This will run the callback for each column to render
+   * the column's data.
+   *
+   * @hooked manage_{$post_type}_posts_custom_column.
+   */
+  public function render_columns( $col_key, $post_id ) {
+    foreach ( $this->cols_to_add as $col ) {
+
+      $func = __NAMESPACE__ .'\\'. $col['callback'];
+
+      if ( !function_exists($func) ) {
+        break;
+      }
+
+      if ( $col_key === $col['key'] ) {
+        call_user_func( $func, $post_id );
+        break;
+      }
+    }
+  }
+
+  /**
+   * Pushes columns to the $cols_to_unset[] class variable.
+   *
+   * @param array | $cols - an indexed array of column keys.
+   */
+  public function unset_columns( $cols ) {
+    foreach ( $cols as $col ) {
+      $this->cols_to_unset[] = $col;
+    }
+  }
+
+  /**
+   * Make custom columns sortable.
+   * This simply enables sortability, but does not control the query.
+   *
+   * @hooked manage_edit-{$post_type}_sortable_columns.
+   */
+  public function make_columns_sortable( $cols ) {
+    foreach ( $this->cols_to_add as $col ) {
+      if ( isset($col['sortable']) && $col['sortable'] === 1 ) {
+        $col_key = $col['key'];
+        $cols[$col_key] = $col_key;
+      }
+    }
+    return $cols;
+  }
+
+  /**
+   * Pre get posts filter for sortable columns.
+   * This is the logic that handles how the sorting actually works.
+   *
+   * @hooked pre_ge_posts.
+   */
+  public function admin_query_ordering( $query ) {
+
+    if ( !is_admin() ) {
+      return;
+    }
+
+    $orderby = $query->get('orderby');
+
+    // $col['sort_key'] should be the meta_key you want to sort by.
+    foreach ( $this->cols_to_add as $col ) {
+      if ( $orderby === $col['key'] ) {
+        $query->set('meta_key', $col['sort_key']);
+        $query->set('orderby', 'meta_value');
+      }
+    }
+  }
+
+}
